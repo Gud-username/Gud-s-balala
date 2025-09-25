ad9655d4657c12033bd62545d4292e037d7413d4
Lusty joker <toby.hayter10@gmail.com>
2025-09-25 19:58:48 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ SMODS.current_mod.extra_tabs = function()
     return {
         {
             label = "Credits",
-            tab_definition_function = vallkarri.credits_ui_def
+            tab_definition_function = gudsbalala.credits_ui_def
         },
     }
 end
